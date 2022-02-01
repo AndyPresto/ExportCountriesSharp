042374fdab546b540d9c4dd5c49d580cb5d7b467
Andrew Preston <andy@assetpad.app>
2022-02-01 13:27:44 +0700

    Get list of countries and export code

---
+++ Countries/Countries.cs
@@ -0,0 +1,17 @@
+﻿using ISO3166;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportCountriesSharp.Countries
+{
+    internal class Countries
+    {
+        public List<Country> GetAll()
+        {
+            return Country.List.ToList();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExportCountriesSharp.Countries;
+
+Console.WriteLine("Starting");
+
+var countriesAll = new Countries().GetAll();
+int exportFormatCode;
+
+Console.WriteLine("Please choose an export format");
+Console.WriteLine("1: JSON");
+Console.WriteLine("2: Source Code");
+Console.WriteLine("Anthing else: Exit");
+
+try
+{
+    exportFormatCode = Convert.ToInt32(Console.ReadLine());
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Exiting");
+}
+
+
+
+Console.WriteLine("Finished, press any key to exit");
